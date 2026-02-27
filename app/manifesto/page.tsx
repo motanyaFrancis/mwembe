0e12e85861cd1d5d252f96632ae86f6f2b1ba48b
motanyafrancis <francismotanya@outlook.com>
2026-02-27 10:57:10 +0300

    font weight changes

--- app/manifesto/page.tsx
+++ app/manifesto/page.tsx
@@ -121,7 +121,7 @@ export default function ManifestoArticle() {
 
                 <div className="absolute max-w-7xl mx-auto inset-0  flex flex-col justify-end sm:justify-center items-left text-left px-0 sm:px-4">
                     <div className="text-left py-12 pb-4 px-8 bg-gradient-to-b from-transparent via-gold-800/30 to-gold-900 sm:bg-none">
-                        <h1 className="text-4xl md:text-5xl font-extrabold text-shadow-md text-shadow-white sm:text-shadow-none text-black leading-tight">
+                        <h1 className="text-4xl md:text-5xl text-shadow-md text-shadow-white sm:text-shadow-none font-black leading-tight">
                             Integrity. Inclusion. Impact.
                         </h1>
                         <h2 className="text-base md:text-2xl text-white md:text-dark-600 font-black mt-2">
